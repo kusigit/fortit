305bffabeb0f86a205a1f700cdcae38ae854ae5e
Markus Fürer <m.fuerer@vtxmail.ch>
2021-12-27 18:49:07 +0100

    fix: 🚀 compile all ts files

--- fwc-card/fwc-card.js
+++ fwc-card/fwc-card.js
@@ -27,12 +27,18 @@ FwcCard.styles = css `
       display: flex;
       justify-content: space-between;
       align-items: center;
+    }
+
+    slot[name='header'][padded] {
       padding: 12px var(--padding-medium);
     }
 
     slot[name='content'] {
       display: block;
-      padding: 0 var(--padding-medium) 0 var(--padding-medium);
+    }
+
+    slot[name='content'][padded] {
+      padding: 0 var(--padding-medium);
     }
 
     .card-action {
