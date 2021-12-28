529e1e99bef334e116ed3327961fddf598f379f5
Markus Fürer <m.fuerer@vtxmail.ch>
2021-12-28 22:52:48 +0100

    fix: 🐛 remove unused exports

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
 export { dynMsg, formatDate } from './base.js';
-
-export * from './fwc-chip/fwc-chip';
-export * from './fwc-chip-list/fwc-chip-list';
-export * from './fwc-data-table/fwc-data-table';
-export * from './fwc-main/fwc-main';
